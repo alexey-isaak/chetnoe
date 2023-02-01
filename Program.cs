92deee095d0531782a38afc24e4a8edce2ccf86f
Alex Isaak <gafar@xaker.ru>
2023-02-01 19:23:50 +0300

    itogovy script

--- Program.cs
+++ Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+ 
+namespace chetnoe
+{
+    class Program
+    {
+        static void Main()
+        {
+Console.Write("Ведите число: ");
+int a = int.Parse(Console.ReadLine());
+double x = a % 2;
+if ( x == 0)
+{
+    Console.WriteLine("Число четное");
+}
+else
+{
+    Console.WriteLine("Число нечетное");
+}
+      }
+       }
+}
